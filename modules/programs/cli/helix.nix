aecba20ad2d40d52774e4aba817a99ce0eb45ca5
nixzoid <nixzoid@jetpure.org>
2025-05-16 18:00:59 +0900

    hel;

--- modules/programs/cli/helix.nix
+++ modules/programs/cli/helix.nix
@@ -78,28 +78,27 @@
           };
 
           file-picker.hidden = false;
+        };
+        keys = {
+          normal = {
+            y = "yank";
+            Y = "yank_to_clipboard";
+            D = "delete_selection_noyank";
+            g.q = ":buffer-close";
 
-          keys = {
-            normal = {
-              y = "yank";
-              Y = "yank_to_clipboard";
-              D = "delete_selection_noyank";
-              g.q = ":buffer-close";
+            space = {
+              space = "file_picker";
+              c = "toggle_comments";
+              f = "file_picker_in_current_directory";
+              F = "file_picker";
 
-              space = {
-                space = "file_picker";
-                c = "toggle_comments";
-                f = "file_picker_in_current_directory";
-                F = "file_picker";
-
-                b = {
-                  b = "buffer_picker";
-                  n = ":buffer-next";
-                  p = ":buffer-previous";
-                  q = ":buffer-close";
-                  Q = ":buffer-close!";
-                  O = ":buffer-close-others";
-                };
+              b = {
+                b = "buffer_picker";
+                n = ":buffer-next";
+                p = ":buffer-previous";
+                q = ":buffer-close";
+                Q = ":buffer-close!";
+                O = ":buffer-close-others";
               };
             };
           };
